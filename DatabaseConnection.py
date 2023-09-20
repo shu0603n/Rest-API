3a370b89b3cbfab5e808f42e8fcc2b31ad498c1e
shunsuke murai <shu0603n@gmail.com>
2023-09-20 22:52:15 +0900

    fastApi

---
+++ DatabaseConnection.py
@@ -0,0 +1,64 @@
+import pymysql
+import paramiko
+from sshtunnel import SSHTunnelForwarder
+
+class DatabaseConnection:
+    def __init__(self):
+        # SSH接続情報
+        self.ssh_host = 'sv14591.xserver.jp'
+        self.ssh_port = 10022
+        self.ssh_username = 'xs739875'
+        self.ssh_key_file = './.ssh/xs739875.key'
+
+        # MySQLデータベース接続情報
+        self.mysql_host = '127.0.0.1'  # ローカルホスト（SSHトンネルを介して接続）
+        self.mysql_port = 3306
+        self.mysql_user = 'xs739875_demo'
+        self.mysql_password = 'adminadmin'
+        self.mysql_db = 'xs739875_demo'
+
+        self.ssh_key = paramiko.RSAKey.from_private_key_file(self.ssh_key_file)
+        self.tunnel = None
+        self.connection = None
+
+    def connect(self):
+        # SSHトンネルの設定
+        self.tunnel = SSHTunnelForwarder(
+            (self.ssh_host, self.ssh_port),
+            ssh_username=self.ssh_username,
+            ssh_pkey=self.ssh_key,
+            remote_bind_address=(self.mysql_host, self.mysql_port)
+        )
+        self.tunnel.start()
+
+        # MySQLへの接続
+        self.connection = pymysql.connect(
+            host='localhost',
+            port=self.tunnel.local_bind_port,
+            user=self.mysql_user,
+            passwd=self.mysql_password,
+            db=self.mysql_db
+        )
+
+    def execute_query(self, query):
+        if not self.connection:
+            self.connect()
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+
+    def execute_update(self, query):
+        if not self.connection:
+            self.connect()
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()  # データベースの変更をコミット
+        cursor.close()
+
+    def close(self):
+        if self.connection:
+            self.connection.close()
+        if self.tunnel:
+            self.tunnel.stop()
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from fastapi import FastAPI
+from DatabaseConnection import DatabaseConnection
 
 app = FastAPI()
+# DatabaseConnectionクラスのインスタンスを作成
+db_connection = DatabaseConnection()
+
+@app.on_event("startup")
+async def startup_event():
+    # アプリケーション起動時にデータベースに接続
+    db_connection.connect()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    # アプリケーション終了時にデータベース接続を閉じる
+    db_connection.close()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/skill/")
+def read_item():
+    # SQLクエリを実行
+    query = "SELECT * FROM skill"
+    result = db_connection.execute_query(query)
+    
+    # 結果をJSON形式で返す
+    return {result}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+
+
+
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
